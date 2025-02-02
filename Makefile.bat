edc6a95e333db30dad695c71e89968c7a131c1f0
Windows <ksm.emails@gmail.com>
2025-02-02 18:23:57 +0100

    refactor

--- Makefile.bat
+++ Makefile.bat
@@ -5,16 +5,17 @@ C:/work/python39/python.exe -m stats
 
 C:/work/python39/python.exe -m traditional.k_means
 C:/work/python39/python.exe -m traditional.k_nearest_neighbors 
-C:/work/python39/python.exe -m traditional.svm
-C:/work/python39/python.exe -m traditional.pca
-C:/work/python39/python.exe -m traditional.gmm
+C:/work/python39/python.exe -m traditional.support_vector_machine
+C:/work/python39/python.exe -m traditional.principal_component_analysis
+C:/work/python39/python.exe -m traditional.gaussian_mixture_model
 C:/work/python39/python.exe -m traditional.logistic_regression
 C:/work/python39/python.exe -m traditional.linear_regression
 C:/work/python39/python.exe -m traditional.naive_bayese
 C:/work/python39/python.exe -m traditional.tree_clustering
-C:/work/python39/python.exe -m traditional.adaboost
-C:/work/python39/python.exe -m traditional.gbm
-C:/work/python39/python.exe -m traditional.xgboost
+C:/work/python39/python.exe -m traditional.singular_value_decompositions
+C:/work/python39/python.exe -m traditional.ada_boost
+C:/work/python39/python.exe -m traditional.gradient_boosting
+C:/work/python39/python.exe -m traditional.xg_boost
 C:/work/python39/python.exe -m traditional.decision_trees
 C:/work/python39/python.exe -m traditional.random_forest
 
@@ -23,10 +24,10 @@ C:/work/python39/python.exe -m neural.code_mse_sigmoid
 C:/work/python39/python.exe -m neural.code_mle_softmax
 C:/work/python39/python.exe -m neural.code_bayes_montecarlo_likelihood
 
-C:/work/python39/python.exe -m generation.vae
-C:/work/python39/python.exe -m generation.gan
-C:/work/python39/python.exe -m generation.diffusion
-C:/work/python39/python.exe -m generation.auto_reg_cnn
-C:/work/python39/python.exe -m generation.auto_reg_lstm
+C:/work/python39/python.exe -m generation.variational_autoencoder
+C:/work/python39/python.exe -m generation.generative_adversarial_network
+C:/work/python39/python.exe -m generation.diffusion_model
+C:/work/python39/python.exe -m generation.autoregressive_cnn
+C:/work/python39/python.exe -m generation.autoregressive_lstm
 
 cd ..
